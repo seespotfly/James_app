4667af63f1784c0bb866873fb60653d2e659713f
seespotfly <adamvhill@gmail.com>
2013-09-30 18:58:57 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JamesApp::Application.config.secret_key_base = '9d113da53fc372144ada8fe0aad9ef782e954f71aee71beb66e423b50c9833ea767167fe681b59d95bc2325cb14b355243b92079f162f29faa1c53789230951e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JamesApp::Application.config.session_store :cookie_store, key: '_James_app_session'
