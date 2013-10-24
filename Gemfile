a98883ccc4594e5792c03a7302a48c8b9d10ec9c
seespotfly <adamvhill@gmail.com>
2013-10-24 19:33:02 -0400

    Placating heroku, lots of other stuff to prep for testing

--- Gemfile
+++ Gemfile
@@ -3,9 +3,6 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
 
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
-
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
@@ -63,3 +60,5 @@ group :test do
   gem "factory_girl_rails", "4.1.0"
   gem "forgery"
 end
+
+gem "twilio-ruby"
---
+++ send_sms.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'twilio-ruby'
+ 
+account_sid = "ACfe33220d3870980954f8d788d6632c7b"
+auth_token = "071a19b24d428604c0a4f7ba4cb5b06b"
+client = Twilio::REST::Client.new account_sid, auth_token
+ 
+from = "+14159998888" # Your Twilio number
+#from = "+19802482173" # Your Twilio number
+ 
+friends = {
+"+14153334444" => "Curious George",
+"+14155557775" => "Boots",
+"+14155551234" => "Virgil"
+}
+friends.each do |key, value|
+  client.account.sms.messages.create(
+    :from => from,
+    :to => key,
+    :body => "Hey #{value}, Monkey party at 6PM. Bring Bananas!"
+  ) 
+  puts "Sent message to #{value}"
+end
