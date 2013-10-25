5e61b7f5bff093959c0c21708013716fb7d69e9f
seespotfly <adamvhill@gmail.com>
2013-10-24 20:08:59 -0400

    Rails port

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,15 @@
 class WelcomeController < ApplicationController
   def index
+  sender = params[:From]
+  friends = {
+    "+17046047104" => "Adam Hill",
+#    "+14158157775" => "Boots",
+#    "+14155551234" => "Virgil"
+  }
+  name = friends[sender] || "Mobile Monkey"
+  twiml = Twilio::TwiML::Response.new do |r|
+    r.Message "Hello, #{name}. Thanks for the message."
   end
+render :text => twiml.text
+end
 end
--- sms_quickstart.rb
+++ sms_quickstart.rb
@@ -3,8 +3,15 @@ require 'twilio-ruby'
 require 'sinatra'
  
 get '/sms-quickstart' do
+  sender = params[:From]
+  friends = {
+    "+17046047104" => "Adam Hill",
+#    "+14158157775" => "Boots",
+#    "+14155551234" => "Virgil"
+  }
+  name = friends[sender] || "Mobile Monkey"
   twiml = Twilio::TwiML::Response.new do |r|
-    r.Message "Hey Monkey. Thanks for the message!"
+    r.Message "Hello, #{name}. Thanks for the message."
   end
   twiml.text
 end
