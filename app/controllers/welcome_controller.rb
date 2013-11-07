e931a3517ef2ae0ca8396a1a30ec1eb36202ca1c
seespotfly <adamvhill@gmail.com>
2013-11-06 21:22:54 -0500

    Adding 12factor to see log errors

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,17 +1,17 @@
 class WelcomeController < ApplicationController
   def index
-  sender = params[:From]
-  friends = {
-    "+17046047104" => "Adam Hill",
-    "+17046040698" => "Katy Hill",
+    sender = params[:From]
+    friends = {
+      "+17046047104" => "Adam Hill",
+      "+17046040698" => "Katy Hill"
 #    "+14158157775" => "Boots",
 #    "+14155551234" => "Virgil"
-  }
-  name = friends[sender] || "Mobile Monkey"
-  twiml = Twilio::TwiML::Response.new do |r|
-    r.Message ParkingCode.message_for(Date.today) 
+    } 
+    name = friends[sender] || "Mobile Monkey"
+    twiml = Twilio::TwiML::Response.new do |r|
+      r.Message ParkingCode.message_for(Date.today) 
 #    r.Message "Hello, #{name}. Thanks for the message."
+    end
+    render :text => twiml.text
   end
-render :text => twiml.text
-end
 end
