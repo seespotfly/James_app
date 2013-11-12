8414b8793b2e2f488600c38c55a9b2fe83a6e5a9
seespotfly <adamvhill@gmail.com>
2013-11-11 20:52:30 -0500

    reply only to usersX10

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,17 @@ class User < ActiveRecord::Base
   validates :name, uniqueness: true
   validates :phone_number, presence: true
   validates :phone_number, uniqueness: true
-  validates_numericality_of :phone_number, only_integer: true,
-    :message => "can only contain numbers, no dashes or parentheses are allowed"
-  validates_length_of :phone_number, :maximum => 11
-  validates_length_of :phone_number, :minimum => 11,
+#  validates_numericality_of :phone_number, only_integer: true,
+#    :message => "can only contain numbers, no dashes or parentheses are allowed"
+#  validates_length_of :phone_number, :maximum => 11
+#  validates_length_of :phone_number, :minimum => 11,
     :message => "must start with +1 followed by area code then number"
   validates :relationship, presence: true
 
 #all my validations are happy!
 
   def self.find_sender(phone_number)
-    User.where(phone_number: "+"+phone_number).first
+    User.where(phone_number: phone_number).first
   end
 end
 
