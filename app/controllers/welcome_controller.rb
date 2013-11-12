76a7d8cabe789412fba1333406943ed181159555
seespotfly <adamvhill@gmail.com>
2013-11-11 20:39:52 -0500

    reply only to usersX6

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
 
     user = User.find_sender(sender)
 
-    if user.nil?
+    if user[sender].nil?
       render :text => sms_message("You are not registered to request parking codes.")
 
     else
