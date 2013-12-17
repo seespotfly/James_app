ad6f62337998f2669c842a5ae067ff9eb115066d
seespotfly <adamvhill@gmail.com>
2013-12-16 21:15:16 -0500

    Attempting a code limit V2

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,7 +10,7 @@ class WelcomeController < ApplicationController
       render :text => sms_message("You are not registered to request parking codes.")
 
 #NEW CODE warning if code limit is reached
-    elsif user.code_limit?
+    elsif user.text_limit?
     render :text => sms_message("You've reached your limit of free codes for the month.")
 
     else
