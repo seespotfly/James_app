972fc478d26661a91a5bc36cb0d2ecea7838a05d
bigfleet <jim@jimvanfleet.com>
2014-04-16 09:13:52 -0400

    Facelift and Rails upgrade

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20131120235704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "parking_codes", force: true do |t|
     t.integer  "code"
